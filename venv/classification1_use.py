20da837f9c586dd3f168efbef35e44cf62dd1089
DW-Han <100533592+DW-Han@users.noreply.github.com>
2023-04-20 14:25:33 -0400

    1

--- venv/classification1_use.py
+++ venv/classification1_use.py
@@ -22,7 +22,7 @@ import csv
 from io import BytesIO
 
 def predict(blob):
-    loaded_model = pickle.load(open('classification1_model.sav', 'rb'))
+    loaded_model = pickle.load(open('C:\\Users\\kenny\\Downloads\\models\\classification1_model.sav', 'rb'))
 
     bytes_obj = bytes(blob)
     # Create a BytesIO object using the bytes object
@@ -31,12 +31,29 @@ def predict(blob):
     # Use PIL to open the image from the BytesIO object
     img_file = Image.open(bytes_io)
     original_image = np.array(img_file)
+
+    # Extract the alpha channel as a NumPy array
+    image = img_file
+    alpha = np.array(image.split()[-1])
+
+    # Create a binary mask of the non-white pixels
+    mask = alpha != 255
+
+    # Create a new alpha channel with transparency for the non-white pixels
+    new_alpha = np.zeros_like(alpha)
+    new_alpha[mask] = 255
+
+    # Replace the old alpha channel with the new one
+    image.putalpha(Image.fromarray(new_alpha))
+
+    # Save the resulting image
+    image.save("output_image.png")
     # get original image parameters...
     width, height = img_file.size
     format = img_file.format
     mode = img_file.mode
 
-    img_file = img_file.resize([28,28])
+    img_file = image.resize([28,28])
 
     # Make image Greyscale
     img_file = img_file.convert('L')
@@ -47,6 +64,19 @@ def predict(blob):
     value = np.asarray(img_file.getdata(), dtype=int).reshape(1, -1)
     #value = pd.DataFrame(value)
     #value = value.values.reshape(28, 28, 1)
+
+    '''img_array = np.zeros((value.shape[1], 4), dtype=np.uint8)
+    img_array[:, :3] = value.T[:, :3]
+    img_array[:, 3] = 255
+
+    # Create mask of non-white pixels
+    non_white_mask = np.any(img_array[:, :3] != [255, 255, 255], axis=-1)
+
+    mask = img_array != 255
+    # Create an RGBA image with alpha channel set to 0 for the background and 255 for the foreground
+   
+    # Set alpha channel to one for non-white pixels
+    img_array[non_white_mask, 3] = 0'''
 
     y_predict = loaded_model.predict(value)
 
